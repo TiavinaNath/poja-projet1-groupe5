1ab5cdecad26d698fcb47115aecb69a1515162d6
TiavinaNath <hei.tiavina.3@gmail.com>
2025-07-16 09:26:52 +0300

    feat: add EnvController

---
+++ src/main/java/com/hei/school/endpoint/rest/controller/EnvController.java
@@ -0,0 +1,20 @@
+package com.hei.school.endpoint.rest.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EnvController {
+
+    private final String secretKey;
+
+    public EnvController(@Value("${SECRET_KEY}") String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    @GetMapping("/env/secret-key")
+    public String getSecretKey() {
+        return secretKey;
+    }
+}
