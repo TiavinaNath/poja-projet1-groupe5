cbc001527aa35f2080fcef6d996534672d6ef396
TiavinaNath <hei.tiavina.3@gmail.com>
2025-07-16 14:45:20 +0300

    fix: use @Value instead getenv

--- src/test/java/com/hei/school/endpoint/rest/controller/EnvControllerTest.java
+++ src/test/java/com/hei/school/endpoint/rest/controller/EnvControllerTest.java
@@ -2,26 +2,24 @@ package com.hei.school.endpoint.rest.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.hei.school.conf.FacadeIT;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class EnvControllerTest {
+@TestPropertySource(properties = "SECRET_KEY=${secret.key}")
+class EnvControllerTest extends FacadeIT {
 
   @LocalServerPort private int port;
 
   private final TestRestTemplate restTemplate = new TestRestTemplate();
 
-  private static final String secretKey = System.getenv("SECRET_KEY");
-
-  @DynamicPropertySource
-  static void overrideSecretKey(DynamicPropertyRegistry registry) {
-    registry.add("SECRET_KEY", () -> secretKey);
-  }
+  @Value("${secret.key}")
+  private String secretKey;
 
   @Test
   void can_read_secret_key() {
