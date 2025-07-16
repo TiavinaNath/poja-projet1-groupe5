0901a9a67b9988e2baf3c5c5b5cc28280b31f7c8
TiavinaNath <hei.tiavina.3@gmail.com>
2025-07-16 15:21:00 +0300

    fix: use @Value instead getenv

--- src/main/java/com/hei/school/endpoint/rest/controller/EnvController.java
+++ src/main/java/com/hei/school/endpoint/rest/controller/EnvController.java
@@ -1,15 +1,15 @@
 package com.hei.school.endpoint.rest.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EnvController {
-
   private final String secretKey;
 
-  public EnvController() {
-    this.secretKey = System.getenv("SECRET_KEY");
+  public EnvController(@Value("${secret.key}") String secretKey) {
+    this.secretKey = secretKey;
   }
 
   @GetMapping("/env/secret-key")
