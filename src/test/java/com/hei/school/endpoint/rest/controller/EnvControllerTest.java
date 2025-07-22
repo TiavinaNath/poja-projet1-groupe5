65ee506486c517728261d9d5a41e4c2d570a1c85
TiavinaNath <hei.tiavina.3@gmail.com>
2025-07-22 13:53:04 +0300

    fixup

--- src/test/java/com/hei/school/endpoint/rest/controller/EnvControllerTest.java
+++ src/test/java/com/hei/school/endpoint/rest/controller/EnvControllerTest.java
@@ -4,12 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.hei.school.conf.FacadeIT;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 class EnvControllerTest extends FacadeIT {
-
+  private static final Logger log = LoggerFactory.getLogger(EnvControllerTest.class);
   @Autowired private TestRestTemplate restTemplate;
 
   @Value("${secret.key:__MISSING__}")
@@ -17,8 +19,10 @@ class EnvControllerTest extends FacadeIT {
 
   @Test
   void can_read_secret_key() {
+    log.info("==> secret.key depuis @Value : {}", secretKey);
     String response = restTemplate.getForObject("/env/secret-key", String.class);
 
+    log.info("==> secret.key depuis l'endpoint : {}", response);
     assertThat(secretKey)
         .withFailMessage("La variable d'environnement 'secret.key' est manquante ou non injectée.")
         .isNotEqualTo("${secret.key}")
