aed29d7a9c2913d8356fcfaf9b8361a4b92112cd
TiavinaNath <hei.tiavina.3@gmail.com>
2025-07-22 14:36:20 +0300

    promotion: preprod to prod

--- src/test/java/com/hei/school/endpoint/rest/controller/EnvControllerTest.java
+++ src/test/java/com/hei/school/endpoint/rest/controller/EnvControllerTest.java
@@ -4,30 +4,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.hei.school.conf.FacadeIT;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestPropertySource(properties = "SECRET_KEY=${secret.key}")
 class EnvControllerTest extends FacadeIT {
 
-  @LocalServerPort private int port;
-
-  private final TestRestTemplate restTemplate = new TestRestTemplate();
+  @Autowired private TestRestTemplate restTemplate;
 
   @Value("${secret.key}")
   private String secretKey;
 
   @Test
   void can_read_secret_key() {
-    String response =
-        restTemplate.getForObject("http://localhost:" + port + "/env/secret-key", String.class);
+    String response = restTemplate.getForObject("/env/secret-key", String.class);
 
     assertThat(secretKey)
-        .withFailMessage("La variable d'environnement SECRET_KEY n'est pas définie.")
+        .withFailMessage("La variable d'environnement secret.key n'est pas définie.")
         .isNotNull();
 
     assertThat(response).isEqualTo(secretKey);
